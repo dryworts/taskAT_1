7dfb1b44b3bba1031889fb24107522ff8ab96cac
dryworts <omzg@mail.ru>
2023-10-10 18:52:57 +0600

    branch testng

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,13 +1,13 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class CashbackHackServiceTest {
 
     @Test
-    void shouldCountCorrent() {
+    public void shouldCountCorrect() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 900;
 
