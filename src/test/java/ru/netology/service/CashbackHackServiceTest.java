3e1d325c1fc001389a4e996b13667b5ed9c7b23b
dryworts <omzg@mail.ru>
2023-10-11 18:28:15 +0600

    tests added in testng branch

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,13 +7,33 @@ import static org.testng.Assert.assertEquals;
 class CashbackHackServiceTest {
 
     @Test
-    public void shouldCountCorrect() {
+    public void shouldCountCorrectIfLess() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 900;
 
         int actual = cashbackHackService.remain(amount);
         int expected = 100;
 
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void shouldCountCorrectIfEqual() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void shouldCountCorrectIfMore() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1100;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 900;
+
+        assertEquals(actual, expected);
     }
 }
